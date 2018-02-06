55dd534eeb58f1d94710f40d776c97f0717fac4c
Martin Falatic <martin@falatic.com>
2018-02-05 23:12:27 -0800

    Update with an Arduino-ESP32 example

--- README.md
+++ README.md
@@ -3,11 +3,14 @@
 ## Disclaimer:
 **I am not liable for any damage this may cause to your Dotti** - No changes are made to the Dotti's firmware, and my experience has been that any transient problems are easily rectified by powering down the Dotti for a few minutes (as well as resetting or rebooting the Linux device that's trying to communicate with the Dotti). The official (and unoffical) apps seem to continue to work just fine regardless of all that, once you power cycle the Dotti.
 
-**I am not affiliated with Witti** - I bought this (on sale!) because I figured since there's already an opensource Android app out there for it (https://github.com/akinaru/dotti-bluetooth-android) that it just might be possible to make this work with the RPi2 - my bet paid off!
+**I am not affiliated with Witti** - I bought this (on sale!) because I figured since there's already an opensource Android app out there for it (https://github.com/bertrandmartel/dotti-bluetooth-android) that it just might be possible to make this work with the RPi2 - my bet paid off!
 
-## Video of the first example in action!
+## Video of the first (RPi) example in action!
 
 <a href="http://www.youtube.com/watch?feature=player_embedded&v=dHzONPPgIac" target="_blank"><img src="http://img.youtube.com/vi/dHzONPPgIac/0.jpg" alt="IMAGE ALT TEXT HERE" width="240" height="180" border="10" /></a>
+
+## Arduino-ESP32
+Use the latest [Arduino-ESP32](https://github.com/espressif/arduino-esp32) from the `master` branch. Build the appropriate demo and load it. It runs automatically.
 
 ## Raspberry Pi + Python Interfacing
 The Python part of this project leverages the bluepy project to interface to the Witti Dotti device from a Linux device (e.g., the Raspberry Pi 2)
@@ -32,7 +35,8 @@ The Python part of this project leverages the bluepy project to interface to the
  - If it doesn't, ping me... I'm still learning how to communicate with this thing.
 
 ### Notes
- - Writing to the Dotti is slow, and I'm bypassing the callbacks to speed it up - this works to a point, but refresh rates are really low (if you go much faster then you end up having to reboot your RPi as well as the Dotti to get it back to connecting)
- - The Dotti is a neat device - and neater still when it's on sale!
+ - Writing to the Dotti is slow, and I'm bypassing the callbacks to speed it up - this works to a point, but refresh rates are really low (if you go much faster then you end up having to reboot your host as well as the Dotti to get it back to connecting).
+ - The Dotti is a neat device - but it's neater still when it's on sale!
+ - The internals are a TI CC2540F256 MPU for the BLE interface, a TTP234-based touch input, and an unknown driver for the 32-pin RGB LED pack. Taking one apart is a royal pain, but necessary if, like mine, one or more of the LED pack's solder joints are cold or completely disconnected.
 
 
---
+++ arduino-esp32/dotti-esp32/dotti-esp32.ino
@@ -0,0 +1,139 @@
+/*
+   Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleScan.cpp
+   Ported to Arduino ESP32 by Evandro Copercini
+*/
+
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEScan.h>
+#include <BLEAdvertisedDevice.h>
+#include <cstring>
+
+
+int scanTime = 30; //In seconds
+BLEAdvertisedDevice currentDevice;
+BLEScan * pBLEScan;
+const char targetName [] = "Dotti";
+bool deviceFound = false;
+
+// The remote service we wish to connect to.
+static BLEUUID serviceUUID("0000fff0-0000-1000-8000-00805f9b34fb");
+// The characteristic of the remote service we are interested in.
+static BLEUUID    charUUID("0000fff3-0000-1000-8000-00805f9b34fb");
+
+static BLEAddress *pServerAddress;
+static boolean connected = false;
+static BLERemoteCharacteristic* pRemoteCharacteristic;
+
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+  void onResult(BLEAdvertisedDevice advertisedDevice) {
+    if (advertisedDevice.haveServiceUUID() && !std::strcmp(advertisedDevice.getName().c_str(), targetName)) {
+      currentDevice = advertisedDevice;
+      pBLEScan->stop();
+      deviceFound = true;
+    }
+  }
+};
+
+static void notifyCallback(
+  BLERemoteCharacteristic* pBLERemoteCharacteristic,
+  uint8_t* pData,
+  size_t length,
+  bool isNotify) {
+    Serial.print("Notify callback for characteristic ");
+    Serial.print(pBLERemoteCharacteristic->getUUID().toString().c_str());
+    Serial.print(" of data length ");
+    Serial.println(length);
+}
+
+bool connectToServer(BLEAddress pAddress) {
+    Serial.print("Forming a connection to ");
+    Serial.println(pAddress.toString().c_str());
+    
+    BLEClient*  pClient  = BLEDevice::createClient();
+    Serial.println(" - Created client");
+
+    // Connect to the remove BLE Server.
+    pClient->connect(pAddress);
+    Serial.println(" - Connected to server");
+
+    // Obtain a reference to the service we are after in the remote BLE server.
+    BLERemoteService* pRemoteService = pClient->getService(serviceUUID);
+    if (pRemoteService == nullptr) {
+      Serial.print("Failed to find our service UUID: ");
+      Serial.println(serviceUUID.toString().c_str());
+      return false;
+    }
+    Serial.println(" - Found our service");
+
+
+    // Obtain a reference to the characteristic in the service of the remote BLE server.
+    pRemoteCharacteristic = pRemoteService->getCharacteristic(charUUID);
+    if (pRemoteCharacteristic == nullptr) {
+      Serial.print("Failed to find our characteristic UUID: ");
+      Serial.println(charUUID.toString().c_str());
+      return false;
+    }
+    Serial.println(" - Found our characteristic");
+
+    // Read the value of the characteristic.
+    std::string value = pRemoteCharacteristic->readValue();
+    Serial.print("The characteristic value was: ");
+    Serial.println(value.c_str());
+
+    pRemoteCharacteristic->registerForNotify(notifyCallback);
+    return true;
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Scanning...");
+  BLEDevice::init("MyESP32");
+  pBLEScan = BLEDevice::getScan(); //create new scan
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setActiveScan(true); //active scan uses more power, but get results faster
+  BLEScanResults foundDevices = pBLEScan->start(scanTime);
+  Serial.print("Devices found: ");
+  Serial.println(foundDevices.getCount());
+  Serial.println("Scan done!");
+}
+
+void loop() {
+  if (deviceFound) {
+    Serial.printf("Advertised Device: %s \n", currentDevice.toString().c_str());
+    Serial.printf("Name: %s \n", currentDevice.getName().c_str());
+    Serial.printf("Manufacturer: %s \n", currentDevice.getManufacturerData().c_str());
+    Serial.printf("MAC Address: %s \n", currentDevice.getAddress().toString().c_str());
+    Serial.printf("Service UUID: %s \n", currentDevice.getServiceUUID().toString().c_str());
+    delay(1000);
+    pServerAddress = new BLEAddress(currentDevice.getAddress());
+    if (connectToServer(*pServerAddress)) {
+      Serial.println("We are now connected to the BLE Server.");
+      connected = true;
+      int cnt = 0;
+      while(1) {
+        uint8_t pixel_idx = random(0, 64);
+        uint8_t color_r = random(0, 2) * 255;
+        uint8_t color_g = random(0, 2) * 255;
+        uint8_t color_b = random(0, 2) * 255;
+        Serial.printf("%d: Pixel %d: #%02X%02X%02X\n", cnt, pixel_idx, color_r, color_g, color_b);
+        int cmdBytesLen = 6;
+        uint8_t cmdBytes [cmdBytesLen] = {0x07, 0x02, (uint8_t)(pixel_idx + 1), color_r, color_g, color_b};
+        //uint8_t cmdBytes [cmdBytesLen] = {0x06, 0x01, color_r, color_g, color_b};
+        //uint8_t cmdBytes [cmdBytesLen] = {0x06, 0x01, uint8_t(cnt), 0, 0};
+        pRemoteCharacteristic->writeValue(cmdBytes, cmdBytesLen);
+        delay(100);
+        cnt += 1;
+      }
+    } else {
+      Serial.println("We have failed to connect to the server; there is nothin more we will do.");
+    }
+  }
+  else {
+    Serial.printf("Target device not found");
+  }
+  while (true) {
+    delay(2000);
+  }
+}
+
